11d794d3862a10f025a13a171aef54a16e5bf180
“BeneditoRodrigo <rodrigodacunha7@gmail.com>
2022-06-15 00:06:34 -0300

    Implementação do teclado (botões) da calculadora

--- src/br/com/beneditorodrigo/calc/visao/Botao.java
+++ src/br/com/beneditorodrigo/calc/visao/Botao.java
@@ -1,8 +1,20 @@
 package br.com.beneditorodrigo.calc.visao;
 
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 
 @SuppressWarnings("serial")
 public class Botao extends JButton {
 
+	public Botao(String texto, Color cor) {
+		setText(texto);
+		setOpaque(true);
+		setBackground(cor);
+		setFont(new Font("courier", Font.PLAIN, 15));
+		setForeground(Color.WHITE);
+		setBorder(BorderFactory.createLineBorder(Color.BLACK));
+	}
 }
